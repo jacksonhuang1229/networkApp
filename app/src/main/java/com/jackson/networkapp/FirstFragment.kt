a3a405aa1e5d52cf405bc67d8674d3c693ac33a5
jacksonhuang1229 <gamegamelife@gmail.com>
2025-03-19 07:12:35 +0800

    [UI] Remove SecondFragment, Adjust FirstFragment

--- app/src/main/java/com/jackson/networkapp/FirstFragment.kt
+++ app/src/main/java/com/jackson/networkapp/FirstFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.jackson.networkapp.databinding.FragmentFirstBinding
 
 /**
@@ -33,7 +32,8 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            val intent = android.content.Intent(requireContext(), RepoListActivity::class.java)
+            startActivity(intent)
         }
     }
 
